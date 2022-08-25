05a5c41db50831f611eaa1b48bbd6e9a4fee280d
amrutha <amruthar1098@gmail.com>
2022-08-25 07:57:26 +0530

    unit tests case to do for cms

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 # to do  >>  Use logging module instead of print for different levels such as debug, error and info
 # to do >> Replace the DB constants into environment variables and value should be loaded from .env files
+# to do >> custom create a response code / status for db query which fails
 DB_USER = '*********'
 DB_HOST = '*********'
 DB_PASSWORD = '**********'
---
+++ unit_test.py
@@ -0,0 +1,4 @@
+"""
+
+create a unit test cae for each post request using flask unit test framework
+"""
